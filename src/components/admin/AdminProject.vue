05d74cb341be5cc07ce42ab281366d52034e27d0
Chris <design.chrislewis@gmail.com>
2020-03-19 18:46:04 +1300

    CRUD - create project

--- src/components/admin/AdminProject.vue
+++ src/components/admin/AdminProject.vue
@@ -4,27 +4,27 @@
     <div class="project__container">
       <div class="project__item">
         <label for="title">Title</label>
-        <input name="title" type="text" />
+        <input v-model="project.title" name="title" type="text" />
       </div>
       <div class="project__item">
         <label for="body">Body</label>
-        <textarea name="body" type="textField" />
+        <textarea v-model="project.body" name="body" type="textField" />
       </div>
       <div class="project__item">
         <label for="slogan">Slogan</label>
-        <input name="slogan" type="text" />
+        <input v-model="project.slogan" name="slogan" type="text" />
       </div>
       <div class="project__item">
         <label for="desktop">Desktop Image URL</label>
-        <input name="desktop" type="text" />
+        <input v-model="project.desktopImageUrl" name="desktop" type="text" />
       </div>
       <div class="project__item">
         <label for="tablet">Tablet Image URL</label>
-        <input name="tablet" type="text" />
+        <input v-model="project.tabletImageUrl" name="tablet" type="text" />
       </div>
       <div class="project__item">
         <label for="mobile">Mobile Image URL</label>
-        <input name="mobile" type="text" />
+        <input v-model="project.mobileImageUrl" name="mobile" type="text" />
       </div>
       <div class="project__item">
         <label for="link">Website URL</label>
@@ -34,19 +34,59 @@
         <label for="more">More Information</label>
         <textarea name="more" type="textField" />
       </div>
-      <Btn text="Save Project" />
+      <Btn text="Save Project" @click="checkForm" />
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import * as config from "../../../config";
 import Btn from "../button/Btn";
 export default {
   name: "adminProject",
   components: {
     Btn
   },
-  data() {}
+  data: function() {
+    return {
+      project: {
+        title: '',
+        slogan: '',
+        body: '',
+        url: '',
+        desktopImageUrl: '',
+        tabletImageUrl: '',
+        mobileImageUrl: '',
+      }
+    };
+  },
+  methods: {
+    createProject: function() {
+      return axios
+        .post(`${config.apiUrl}/projects`, this.project)
+        .then(() => {
+          this.$router.push({ path: "/admin" });
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    checkForm: function(evt) {
+      evt.preventDefault();
+      this.errors = [];
+      if (!this.project.title) {
+        this.errors.push("Title required");
+      }
+      if (!this.project.body) {
+        this.errors.push("Body required");
+      }
+      if (!this.project.url) {
+        this.errors.push("Link required");
+      }
+      this.createProject();
+    }
+  }
 };
 </script>
 
